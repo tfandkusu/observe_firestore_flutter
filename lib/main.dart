854d9fc03d4f658cce4958220edf2d6f92998e57
Haruhiko Takada <haruhiko.takada@gmail.com>
2020-11-14 23:52:12 +0900

    Create member list with sticky header.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_sticky_header/flutter_sticky_header.dart';
 import 'package:hooks_riverpod/all.dart';
 
 import 'data.dart';
@@ -28,11 +29,12 @@ class MyApp extends StatelessWidget {
 class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Member List"),
-        ),
-        body: MainBody());
+    return DefaultStickyHeaderController(
+        child: Scaffold(
+            appBar: AppBar(
+              title: Text("Member List"),
+            ),
+            body: MainBody()));
   }
 }
 
@@ -45,10 +47,32 @@ class MainBody extends HookWidget {
         builder:
             (BuildContext context, AsyncSnapshot<List<Division>> snapshot) {
           if (snapshot.hasData) {
-            print(snapshot.data.length);
-            return const Center();
+            final headerTextStyle = Theme.of(context)
+                .textTheme
+                .subtitle2
+                .copyWith(color: Colors.white);
+            final headerBackgroundColor =
+                Color.fromARGB(0xff, 0x66, 0x66, 0x66);
+            return CustomScrollView(
+              slivers: snapshot.data
+                  .map((e) => SliverStickyHeader(
+                        header: Container(
+                          child: Text(e.division, style: headerTextStyle),
+                          padding: EdgeInsets.all(16),
+                          decoration:
+                              BoxDecoration(color: headerBackgroundColor),
+                        ),
+                        sliver: SliverList(
+                            delegate: SliverChildBuilderDelegate(
+                                (context, i) => Container(
+                                    child: Text(e.names[i]),
+                                    padding: EdgeInsets.fromLTRB(16, 8, 16, 8)),
+                                childCount: e.names.length)),
+                      ))
+                  .toList(),
+            );
           } else {
-            return const Center(child: CircularProgressIndicator());
+            return Center(child: CircularProgressIndicator());
           }
         });
   }
